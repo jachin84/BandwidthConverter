f2618255b5b16984763a431f488b15acff8c7f38
Jacob Hodges <jachin84@users.noreply.github.com>
2019-09-10 16:02:12 +1000

    Implement initial version

---
+++ BandwidthConverter/BandwidthConverter.psm1
@@ -0,0 +1,58 @@
+﻿
+<#
+.SYNOPSIS
+    Convert bytes to bits per second.
+.DESCRIPTION
+    ConvertTo-BitsPerSecond converts bytes per second to bits per second.
+.EXAMPLE
+    Converts 50 megabyte per second to kilobits per second.
+
+    PS C:\> ConvertTo-BitsPerSecond -BytesPerSecond 50MB -Kbps
+    409600
+
+#>
+function ConvertTo-BitsPerSecond {
+    [CmdletBinding()]
+    param (
+        [Parameter(ParameterSetName = "Kbps")]
+        [Parameter(ParameterSetName = "Mbps")]
+        [Parameter(ParameterSetName = "Gbps")]
+        [Parameter(ParameterSetName = "Tbps")]
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline,
+                   ValueFromPipelineByPropertyName,
+                   Position = 0)]
+        [int]
+        $BytesPerSecond,
+
+        # Kbps
+        [Parameter(Mandatory=$true, ParameterSetName="Kbps")]
+        [Switch]
+        $Kpbs,
+
+        # Mpbs
+        [Parameter(Mandatory=$true, ParameterSetName="Mbps")]
+        [Switch]
+        $Mbps,
+
+        # Gbps
+        [Parameter(Mandatory=$true, ParameterSetName="Gbps")]
+        [Switch]
+        $Gpbs,
+
+        # Tbps
+        [Parameter(Mandatory=$true, ParameterSetName="Tbps")]
+        [Switch]
+        $Tbps
+
+    )
+
+    switch ($PSCmdlet.ParameterSetName) {
+        "Kbps" { $divisor = 1KB }
+        "Mbps" { $divisor = 1MB }
+        "Gbps" { $divisor = 1GB }
+        "Tbps" { $divisor = 1TB }
+    }
+
+    Write-Output ($BytesPerSecond * 8 / $divisor)
+}
---
+++ Tests/BandwidthConverterTests.ps1
@@ -0,0 +1,25 @@
+﻿
+
+Import-Module $PSScriptRoot\..\BandwidthConverter\BandwidthConverter.psd1 -Force
+
+Describe "Bandwidt Converter"{
+    Context "Bits per second" {
+        $size = 50mb
+
+        It "Kbps" {
+            ConvertTo-BitsPerSecond -BytesPerSecond $size -Kpbs | Should -Be 409600
+        }
+
+        It "Mbps" {
+            ConvertTo-BitsPerSecond -BytesPerSecond $size -Mbps | Should -Be 400
+        }
+
+        It "Gbps" {
+            ConvertTo-BitsPerSecond -BytesPerSecond $size -Gpbs | Should -Be 0.390625
+        }
+
+        It "Tbps" {
+            ConvertTo-BitsPerSecond -BytesPerSecond $size -Tbps | Should -Be 0.0003814697265625
+        }
+    }
+}
